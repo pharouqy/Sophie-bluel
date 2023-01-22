5aa5f472bafd3cfb9186729aa23e9aab2dfc0d0d
farouky <farouky@gmail.com>
2023-01-22 12:01:00 +0100

    Start of project

---
+++ FrontEnd/assets/js/script.js
@@ -0,0 +1,65 @@
+const url = "http://localhost:5678/api/";
+
+const gallery = document.querySelector(".gallery");
+const portfolio = document.getElementById("portfolio");
+
+// Create filters for the gallery
+const filters = document.createElement("div");
+filters.classList.add("filters");
+portfolio.insertBefore(filters, gallery);
+
+// function create filters for the gallery
+function createFilters(tag, classes = [], content) {
+  const filter = document.createElement(tag);
+  classes.forEach((classe) => filter.classList.add(classe));
+  filter.textContent = content;
+  filters.appendChild(filter);
+}
+
+createFilters("button", ["filter", "active_filter"], "Tous");
+
+// create filters for the gallery
+fetch(`${url}categories`)
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+  })
+  .then((categories) => {
+    categories.forEach((category) => {
+      createFilters("button", ["filter"], category.name);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+// Create cards
+function card(imgUrl, title, categoryId) {
+  const figure = document.createElement("figure");
+  figure.classList.add("card");
+  figure.dataset.category = categoryId;
+  gallery.appendChild(figure);
+  const img = document.createElement("img");
+  img.crossOrigin = "anonymous";
+  img.src = imgUrl;
+  img.alt = title;
+  const figcaption = document.createElement("figure");
+  figcaption.textContent = title;
+  figure.appendChild(img);
+  figure.appendChild(figcaption);
+}
+
+// Display cards in the gallery
+fetch(`${url}works`)
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+  })
+  .then((projects) => {
+    projects.forEach((project) => {
+      card(project.imageUrl, project.title, project.categoryId);
+    });
+  })
+  .catch((error) => console.log(error));
